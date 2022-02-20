28900c68846279faf2078c3aef02834243592e3f
dor <dorstern3@gmail.com>
2022-02-20 23:07:33 +0200

    header and footer

--- Component/header_mobile.js
+++ Component/header_mobile.js
@@ -2,14 +2,15 @@
 console.log("hello word");
 
 function opennavbar(){
+  
     document.getElementById("navbar").style.display="block";
-    document.getElementById("main").style.marginRight = "200px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
+    document.getElementById("main").style.marginRight = "250px";
+    // document.getElementById("main").style.transition = "margin left 5s.";
 }
 
 function closenavbar(){
     document.getElementById("navbar").style.display="none";
     document.getElementById("main").style.marginRight = "0";
-    document.body.style.backgroundColor = "white";
+    
 
 }

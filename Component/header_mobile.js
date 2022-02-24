8d009d2b6652a1fafb1547683ecaa280ab7b6ea4
dor <dorstern3@gmail.com>
2022-02-24 17:53:25 +0200

    a

--- Component/header_mobile.js
+++ Component/header_mobile.js
@@ -4,8 +4,10 @@ console.log("hello word");
 function opennavbar(){
   
     document.getElementById("navbar").style.display="block";
-    document.getElementById("main").style.marginRight = "250px";
+    document.getElementById("navbar").style.width = "100%";
+    // document.getElementById("main").style.marginRight = "250px";
     // document.getElementById("main").style.transition = "margin left 5s.";
+    
 }
 
 function closenavbar(){

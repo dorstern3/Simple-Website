48431c2fac1284a35bdb54ac776f414e4f8b1f8b
dor <dorstern3@gmail.com>
2022-02-25 22:21:29 +0200

    header mobile

--- Component/header_mobile.js
+++ Component/header_mobile.js
@@ -5,8 +5,8 @@ function opennavbar(){
   
     document.getElementById("navbar").style.display="block";
     document.getElementById("navbar").style.width = "100%";
-    // document.getElementById("main").style.marginRight = "250px";
-    // document.getElementById("main").style.transition = "margin left 5s.";
+
+    
     
 }
 

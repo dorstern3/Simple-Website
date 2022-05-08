1b274c0b5a29d32f0a03d22ad7264d2c8cfe6ff1
dor <dor@DESKTOP-L9NS20G>
2022-05-08 20:44:28 +0300

    a

--- main/back/urls.py
+++ main/back/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.Home,name='Home')
+    path('',views.Home,name='Home'),
+    path('SignUp',views.SignUp,name='SignUp')
     
 ]
--- main/back/views.py
+++ main/back/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render , redirect
 from django.http import HttpResponse
 # Create your views here.
+
+# Home page
 def Home(request):
     return render(request,'Home.html')
+
+# SignUp page
+def SignUp(request):
+    return render(request,'SignUp.html')

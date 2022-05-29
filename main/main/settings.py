e4cb4e64284170a42ed53ebf645782261ba9e7bb
dor <dor@DESKTOP-L9NS20G>
2022-05-29 15:48:38 +0300

    try

--- main/main/settings.py
+++ main/main/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-emdq#zx61+c=)@ceos9ezfktq!k4pwm1#u0zi#^r-ohmmmnml*'
-
+SECRET_KEY = 'a124963fd329012740b657bfd4c43b8818d311068f781aff'
+# SECRET_KEY = 'django-insecure-emdq#zx61+c=)@ceos9ezfktq!k4pwm1#u0zi#^r-ohmmmnml*'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['simple1website.herokuapp.com']
 
 
 # Application definition

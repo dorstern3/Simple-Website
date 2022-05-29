6f9a75f03b2d481900c59b9e31d3a5181c61a9ee
dor <dor@DESKTOP-L9NS20G>
2022-05-29 12:17:50 +0300

    staticfileheroku

--- main/main/settings.py
+++ main/main/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles') #HEROKU
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),
                     os.path.join(BASE_DIR, 'static/Component'),
